0aa289279546b8d97988c4d51a73b63236af4539
saikrishna-prathapaneni <57140923+saikrishna-prathapaneni@users.noreply.github.com>
2020-06-19 10:44:31 +0530

    updated

--- req.sql
+++ req.sql
@@ -4,3 +4,15 @@ CREATE TABLE users (
     Pass VARCHAR NOT NULL
        
 );
+
+
+SELECT email,pass FROM users JOIN bpok_user ON users.email=bpok_user.email
+
+
+CREATE TABLE bpok_user (
+   id SERIAL PRIMARY KEY,
+    email varchar NOT NULL,
+    book_name VARCHAR NOT NULL,
+    book_id INT NOT NULL
+       
+);
